e869875c46221e0eda4133852694ce4861fa4f97
Armine Avdalyan <armineavdalyan85@gmail.com>
2021-02-12 21:28:01 +0400

    Initial boilerplate

---
+++ src/containers/Container/Container.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import App from "../../App";
+
+export class Container extends Component {
+  render() {
+    return (
+      <div>
+        React BoilerPlate
+      </div>
+    )
+  }
+}
+
+export default Container;
